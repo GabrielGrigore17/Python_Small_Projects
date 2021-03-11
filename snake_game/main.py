5b603d4edfdd9e358afa98e969002b8c54d37573
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-03-11 16:10:16 +0200

    Added file manipulation

--- snake_game/main.py
+++ snake_game/main.py
@@ -32,14 +32,14 @@ while game_is_on:
         scoreboard.refresh()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
         screen.update()
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,21 +1,31 @@
 from turtle import Turtle
+FONT = ("Arial", 16, "normal")
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = -1
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(0, 270)
         self.refresh()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(str(self.high_score))
+        self.score = -1
+        self.refresh()
 
     def refresh(self):
         self.score += 1
         self.clear()
-        self.write("Score: {}".format(self.score), move=False, align="center", font=("Arial", 16, "normal"))
+        self.write("Score: {0} High Score {1}".format(self.score, self.high_score), align="center", font=FONT)
