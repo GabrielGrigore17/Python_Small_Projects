8547983e04e2efba316276f04766651c384156d8
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-03-13 14:03:26 +0200

    Extracted data from open source

--- csvFilesReading/main.py
+++ csvFilesReading/main.py
@@ -12,10 +12,32 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
+# import pandas
+
+
+# def f(x):
+#     x = x * 1.8 + 32
+#     return float(x)
+#
+#
+# data = pandas.read_csv("weather_data.csv")
+# # temperatures = data["temp"].to_list()
+# # print(sum(temperatures)/len(temperatures))
+# # print(data["temp"].max())
+# # print(data[data.temp == data.temp.max()])
+# # print(f(data[data.day == "Monday"].temp))
+# print(data.to_dict())
+
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-# temperatures = data["temp"].to_list()
-# print(sum(temperatures)/len(temperatures))
-print(data["temp"].max())
-print(data[data.temp == data.temp.max()])
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+colours = data["Primary Fur Color"].to_list()
+cleaned_colours = [x for x in colours if str(x) != 'nan']
+data_dict = {i: cleaned_colours.count(i) for i in cleaned_colours}
+final_data_dict = {
+    "Colour": list(data_dict.keys()),
+    "No": list(data_dict.values())
+}
+data_output = pandas.DataFrame(final_data_dict)
+data_output.to_csv("data_output.csv")
