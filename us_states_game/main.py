6638de94d0a2470f5f61ad13c96d27403d7e30a0
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-03-15 13:32:36 +0200

    Completed

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -13,18 +13,22 @@ on_screen_text = OnScreenText()
 
 game_is_on = True
 guessed = 0
+guessed_states = []
 while game_is_on:
     screen.update()
     answer_state = screen.textinput(title=f"Guess the state name {guessed}/50", prompt="What's another state name?")
-    if states[states.state == answer_state].empty:
+    answer_state = answer_state.title()
+    if states[states.state == answer_state].empty or answer_state in guessed_states:
         continue
+    guessed_states.append(answer_state)
     guessed += 1
     x_cor = states[states.state == answer_state].x
     y_cor = states[states.state == answer_state].y
-    print(x_cor, y_cor)
     on_screen_text.write_on_screen(int(x_cor), int(y_cor), answer_state)
     if guessed == 50:
         game_is_on = False
+
+
 
 screen.exitonclick()
 
